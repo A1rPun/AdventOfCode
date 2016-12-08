8ff9703111b5605d18edea4d9e9c93981d235688
A1rPun <thombakker@gmail.com>
2016-12-08 22:16:05 +0100

    :cyclone: Fixes

--- js/main.js
+++ js/main.js
@@ -1,55 +1,50 @@
-﻿(function (w) {
-    var code = document.getElementById('code');
-    function logCode(c) {
-        code.innerHTML += (typeof c === 'string' ? c : JSON.stringify(c, null, 4)) + '\n';
+﻿(function (w, d) {
+    var code = d.querySelector('.code');
+    function logCode(c, click) {
+        var str = (typeof c === 'string' ? c : JSON.stringify(c, null, 4));// + '\n'
+        var span = d.createElement('span');
+        span.className = 'line';
+        span.innerText = str;
+        if (click)
+            span.addEventListener('click', click);
+        code.appendChild(span);
     }
-    var timer = {
-        begin: 0,
-        end: 0,
-        start: function () {
-            this.begin = performance.now();
-        },
-        stop: function () {
-            this.end = performance.now() - this.begin;
-        },
-        log: function () {
-            logCode('Execution took ' + this.end + ' milliseconds.');
-        }
-    };
+    
     logCode('AdventOfCode - 2016 - A1rPun');
     logCode('----------------------------');
     var days = December.getDays();
     for (var i = 0; i < days.length; i++) {
-        var day = days[i];
-        logCode('Day ' + (i + 1));
-        logCode('------');
-        if (day.questions) {
-            logCode('Question(s):');
-            logCode(day.questions);
-        }
-        /*
-        if (day.input) {
-            logCode('Input:');
-            logCode(day.input);
-        }
-        */
-        timer.start();
-        var answer = day.answer();
-        timer.stop();
-        if (answer) {
-            logCode('Answer(s):');
-            logCode(answer);
-            timer.log();
-        }
-        timer.start();
-        var example = day.example();
-        timer.stop();
-        if (example) {
-            logCode('Example(s):');
-            logCode(example);
-            timer.log();
-        }
-        logCode('\n');
+        logCode('Day ' + (i + 1), (function (day) {
+            return function () {
+                logCode('------');
+                if (day.questions) {
+                    logCode('Question(s):');
+                    logCode(day.questions);
+                }
+                /*
+                if (day.input) {
+                    logCode('Input:');
+                    logCode(day.input);
+                }
+                */
+                timer.start();
+                var answer = day.answer();
+                timer.stop();
+                if (answer) {
+                    logCode('Answer(s):');
+                    logCode(answer);
+                    logCode(timer.log());
+                }
+                timer.start();
+                var example = day.example();
+                timer.stop();
+                if (example) {
+                    logCode('Example(s):');
+                    logCode(example);
+                    logCode(timer.log());
+                }
+            };
+        }(days[i])));
     }
     var inputs = [];
-}(window));
+}(window, document));
---
+++ js/perfTimer.js
@@ -0,0 +1,13 @@
+﻿var timer = {
+    begin: 0,
+    end: 0,
+    start: function () {
+        this.begin = performance.now();
+    },
+    stop: function () {
+        this.end = performance.now() - this.begin;
+    },
+    log: function () {
+        return 'Execution took ' + this.end + ' milliseconds.';
+    }
+};
