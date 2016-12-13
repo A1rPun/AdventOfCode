e57bf1189c8da4da14a7bf697a1f9d7bbb73d759
A1rPun <thombakker@gmail.com>
2016-12-13 21:59:06 +0100

    :large_blue_circle: Add day 10 & 11

---
+++ 2016/day_11/day_11.js
@@ -0,0 +1,16 @@
+(function () {
+    function day_11(puzzle) {
+        return Promise.resolve();
+    }
+    function getInput() {
+        return [];
+    }
+    December.addDay({
+        day: 11,
+        //title: '',
+        questions: '',
+        answer: day_11,
+        input: getInput,
+        example: function () { return []; }
+    });
+}());
