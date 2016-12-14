d4a2641a017eab9ace95cf5edf5637c6a9ffc4dc
A1rPun <thombakker@gmail.com>
2016-12-14 23:51:20 +0100

    :black_circle: Day 14 worker

--- 2016/day_14/day_14_worker.js
+++ 2016/day_14/day_14_worker.js
@@ -3,6 +3,35 @@ onmessage = function (e) {
     processInput(e.data);
 
     function processInput(salt) {
-        console.log(salt);
+        let numHashes = 0;
+        let keys = [];
+        let valids = [];
+        while (keys.length < 64) {
+            let hash = md5(salt + numHashes).toLowerCase();
+            let occurrences = hash.match(/(\S)\1{2,}/g) || [];
+            for (var i = 0; i < occurrences.length; i++) {
+                var occur = occurrences[i];
+                if (occur.length === 3) {
+                    valids.push({
+                        occur: occur,
+                        index: numHashes
+                    });                    
+                } else if (occur.length === 5) {
+                    var newOccur = occur.slice(-3);
+                    for (var j = valids.length; j--;) {
+                        var valid = valids[j];
+                        if (valid.occur === newOccur) {
+                            valids.splice(j, 1);
+                            if (valid.index + 1000 > numHashes) {
+                                keys.push(numHashes);
+                            }
+                        }
+                    }
+                }
+            }
+            numHashes++;
+        }
+        postMessage(keys[keys.length - 1]);
+        close();
     }
 }
