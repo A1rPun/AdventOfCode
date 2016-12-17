bd67f4bfe4096e2f94ced5cdfa30383d8a73b441
A1rPun <thombakker@gmail.com>
2016-12-17 18:03:08 +0100

    :star: Day 16 complete!

---
+++ 2016/day_16/day_16.js
@@ -0,0 +1,37 @@
+(function () {
+    function dragonCurve(bits) {
+        var result = bits + '0';
+        for (var i = bits.length; i--;)
+            result += bits[i] === '0' ? '1' : '0';
+        return result;
+    }
+
+    function getChecksum(bits) {
+        var result = '';
+        for (var i = 0, l = bits.length; i < l; i += 2)
+            result += bits[i] === bits[i + 1] ? '1' : '0';
+        return result;
+    }
+
+    function day_16(puzzle) {
+        var bits = puzzle[0];
+        var length = puzzle[1];
+        while (bits.length < length) {
+            bits = dragonCurve(bits);
+        }
+        bits = bits.slice(0, length);
+        while (!(bits.length & 1)) {
+            bits = getChecksum(bits);
+        }
+        return Promise.resolve(bits);
+    }
+
+    December.addDay({
+        day: 16,
+        title: 'Dragon Checksum',
+        questions: 'What is the correct checksum?',
+        answer: day_16,
+        input: function () { return ['01111010110010011', 272]; }, // answer 2 = 35651584
+        example: function () { return ['10000', 20]; }
+    });
+}());
