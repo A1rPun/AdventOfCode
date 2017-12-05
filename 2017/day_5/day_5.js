1fdaf141d29abc2af8d46523987ff51ad6518608
A1rPun <thombakker@gmail.com>
2017-12-05 16:14:05 +0100

    :star: Add 2017/4

---
+++ 2017/day_5/day_5.js
@@ -0,0 +1,18 @@
+(function () {
+    function day_5(puzzle) {
+        return Promise.resolve();
+    }
+    December.addDay({
+        day: 5,
+        year: 2017,
+        title: '',
+        questions: '',
+        answer: day_5,
+        input: function () {
+            return [];
+        },
+        example: function () {
+            return [];
+        },
+    });
+}());
