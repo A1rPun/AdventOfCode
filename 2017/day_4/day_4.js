55cf47f0a45513d1887ac6d8c8d442e4c361e819
A1rPun <thombakker@gmail.com>
2017-12-05 15:48:47 +0100

    :cyclone: Already add years to index.html

---
+++ 2017/day_4/day_4.js
@@ -0,0 +1,18 @@
+(function () {
+    function day_4(puzzle) {
+        return Promise.resolve();
+    }
+    December.addDay({
+        day: 4,
+        year: 2017,
+        title: '',
+        questions: '',
+        answer: day_4,
+        input: function () {
+            return [];
+        },
+        example: function () {
+            return [];
+        },
+    });
+}());
