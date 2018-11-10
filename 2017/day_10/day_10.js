9a7a43a34319e9e427401c25c4a3ab196be0a835
Thom Bakker <thombakker@gmail.com>
2018-11-11 00:35:59 +0100

    :fireworks: Completed 2017/10

--- 2017/day_10/day_10.js
+++ 2017/day_10/day_10.js
@@ -1,27 +1,53 @@
 (function () {
-    function day_10(puzzle) {
-        var list = [...Array(puzzle[0]).keys()];
-        var l = list.length;
-        var input = puzzle[1].split(',').map(function (e) { return +e; });
-        var listIndex = 0;
-        var skipSize = 0;
-
-        for (let i = 0; i < input.length; i++) {
-            const knotLength = input[i];
-            const toIndex = listIndex + knotLength;
-            var wrapIndex = toIndex > l ? toIndex - l : -1;
-            let current = (~wrapIndex
-                ? list.slice(listIndex, l + 1).concat(list.slice(0, wrapIndex))
-                : list.slice(listIndex, toIndex)).reverse();
-            list = ~wrapIndex
-                ? current.slice(-wrapIndex).concat(list.slice(wrapIndex, listIndex)).concat(current.slice(0, -wrapIndex))
-                : list.slice(0, listIndex).concat(current).concat(list.slice(toIndex, l + 1));
-            listIndex = (listIndex + knotLength + skipSize) % l;
-            skipSize++;
+    function knotHash(input, times) {
+        let hash = [...Array(256).keys()];
+        const l = hash.length;
+        let hashIndex = 0;
+        let skipSize = 0;
+        while (times--) {
+            for (let i = 0; i < input.length; i++) {
+                const knotLength = input[i];
+                const toIndex = hashIndex + knotLength;
+                var wrapIndex = toIndex > l ? toIndex - l : -1;
+                let current = (~wrapIndex
+                    ? hash.slice(hashIndex, l + 1).concat(hash.slice(0, wrapIndex))
+                    : hash.slice(hashIndex, toIndex)).reverse();
+                hash = ~wrapIndex
+                    ? current.slice(-wrapIndex).concat(hash.slice(wrapIndex, hashIndex)).concat(current.slice(0, -wrapIndex))
+                    : hash.slice(0, hashIndex).concat(current).concat(hash.slice(toIndex, l + 1));
+                hashIndex = (toIndex + skipSize) % l;
+                skipSize++;
+            }
         }
-        const answer1 = list[0] * list[1];
-        return Promise.resolve([answer1]);
+        return hash;
     }
+
+    function dense(hash) {
+        let densedHash = [];
+        let xorred = hash.reduce(function (prev, curr, i) {
+            if (i % 16 === 0) {
+                densedHash.push(prev);
+                return curr;
+            }
+            return prev ^ curr;
+        });
+        densedHash.push(xorred);
+        return densedHash;
+    }
+
+    function day_10(puzzle) {
+        const input1 = puzzle.split(',').map(function (e) { return +e; });
+        const input2 = puzzle.split('').map(function (char) {
+            return char.charCodeAt(0);
+        }).concat([17, 31, 73, 47, 23]);
+        const answer1 = knotHash(input1, 1);
+        const densedHash = dense(knotHash(input2, 64));
+        const answer2 = densedHash.map(function (a) {
+            return a.toString(16).padStart(2, '0');
+        }).join('');
+        return Promise.resolve([answer1[0] * answer1[1], answer2]);
+    }
+
     December.addDay({
         day: 10,
         year: 2017,
@@ -29,16 +55,11 @@
         questions: 'What is the result of multiplying the first two numbers in the list?',
         answer: day_10,
         input: function () {
-            return [
-                256,
-                '147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70',
-            ];
+            //70b856a24d586194331398c7fcfa0aaf
+            return '147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70';
         },
         example: function () {
-            return [
-                5,
-                '3, 4, 1, 5',
-            ];
+            return '1,2,3';
         },
     });
 }());
