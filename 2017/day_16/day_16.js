ff64b96d1aa54250d2c02d65fce20f579d6bed5a
Thom Bakker <thombakker@gmail.com>
2018-11-16 22:06:35 +0100

    :star: 2017 Day 16 answer 1 (WIP)

--- 2017/day_16/day_16.js
+++ 2017/day_16/day_16.js
@@ -1,18 +1,59 @@
 (function () {
-    function day_(puzzle) {
-        return Promise.resolve();
+    const Dance = {
+        x: (arr, a, b) => swap(arr, a, b),
+        p: (arr, a, b) => swap(arr, arr.indexOf(a), arr.indexOf(b)),
+        s: (arr, index) => arr.slice(-index).concat(arr.slice(0, -index)),
+    };
+
+    function swap(arr, a, b) {
+        arr[a] = arr.splice(b, 1, arr[a])[0];
+        return arr;
+    }
+
+    function dance(programs, dance) {
+        return Dance[dance.move](programs, ...dance.value);
+    }
+
+    function parseDance(dance, length) {
+        const parsedDance = {
+            move: dance[0],
+        };
+        switch (dance[0]) {
+            case 'x':
+                parsedDance.value = dance.match(/(\d+)/g);
+                break;
+            case 'p':
+                parsedDance.value = dance.match(/p(\w+)\/(\w+)/).slice(-2);
+                break;
+            default:
+                parsedDance.value = [dance.slice(1) % length];
+                break;
+        }
+        return parsedDance;
+    }
+
+    function day_16(puzzle) {
+        const programs = 'abcdefghijklmnop'.split(''); //'abcde';
+        const dances = puzzle.split(',').map(x => parseDance(x, programs.length));
+        let answer2 = dances.reduce(dance, programs);
+        const answer1 = answer2.join('');
+        /*
+        let max = 999999999;
+        while (max--) {
+            answer2 = dances.reduce(dance, answer2);
+        }
+        return Promise.resolve([answer1, answer2.join('')]);
+        */
+        return Promise.resolve(answer1);
     }
     December.addDay({
-        //day: ,
+        day: 16,
         year: 2017,
-        title: '',
-        questions: '',
-        answer: day_,
-        input: function () {
-            return [];
-        },
+        title: 'Permutation Promenade',
+        questions: ['In what order are the programs standing after their dance?', 'In what order are the programs standing after their billion dances?'],
+        answer: day_16,
         example: function () {
-            return [];
+            return 's1,x3/4,pe/b';
         },
     });
 }());
