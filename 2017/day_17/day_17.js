872da62d8223c8622aff33c922a7b0468006e488
Thom Bakker <thombakker@gmail.com>
2018-11-17 16:04:55 +0100

    :star: 2017 Day 17 part 1 (WIP)

--- 2017/day_17/day_17.js
+++ 2017/day_17/day_17.js
@@ -1,18 +1,27 @@
 (function () {
-    function day_(puzzle) {
-        return Promise.resolve();
+    function day_17(puzzle) {
+        const buffer = [0];
+        const max = 2017;
+        let position = 0;
+        for (let i = 0; i < max;) {
+            position = (position + puzzle) % buffer.length;
+            buffer.splice(++position, 0, ++i);
+        }
+        const answer1 = buffer[buffer.indexOf(max) + 1];
+        return Promise.resolve(answer1);
     }
     December.addDay({
-        //day: ,
+        day: 17,
         year: 2017,
-        title: '',
-        questions: '',
-        answer: day_,
-        input: function () {
-            return [];
-        },
+        title: 'Spinlock',
+        questions: [
+            'What is the value after 2017 in your completed circular buffer?',
+            'What is the value after 0 the moment 50000000 is inserted?'
+        ],
+        answer: day_17,
+        input: 329,
         example: function () {
-            return [];
+            return 3;
         },
     });
 }());
