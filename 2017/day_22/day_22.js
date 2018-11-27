84aeb0c54f7e439546cb3f6b205424b5f22ac0b6
Thom Bakker <thombakker@gmail.com>
2018-11-27 18:34:31 +0100

    :star: 2017 Day 22 part 1

--- 2017/day_22/day_22.js
+++ 2017/day_22/day_22.js
@@ -1,18 +1,47 @@
 (function () {
-    function day_(puzzle) {
-        return Promise.resolve();
+    function day_22(puzzle) {
+        const face = {
+            top: 0,
+            right: 1,
+            bottom: 2,
+            left: 3,
+        };
+        const virus = puzzle.split('\n').reduce((acc, curr, y) => {
+            curr.split('').forEach((val, x) => {
+                if (val === '#')
+                    acc[`${x}_${y}`] = 1;
+            });
+            return acc;
+        }, {});
+
+        let x, y;
+        x = y = Math.floor(Math.sqrt(puzzle.length) / 2);
+        let direction = face.top;
+        let bursts = 10000;
+        let answer1 = 0;
+        while (bursts--) {
+            const key = `${x}_${y}`;
+            const currentNode = virus[key] || 0;
+            direction = (4 + direction + (currentNode ? 1 : -1)) % 4;
+            virus[key] = currentNode ? 0 : answer1++, 1;
+            
+            if (direction === face.top) y--;
+            else if (direction === face.right) x++;
+            else if (direction === face.bottom) y++;
+            else x--;
+        }
+        return Promise.resolve(answer1);
     }
     December.addDay({
-        //day: ,
+        day: 22,
         year: 2017,
-        title: '',
-        questions: '',
-        answer: day_,
-        input: function () {
-            return [];
-        },
+        title: 'Sporifica Virus',
+        questions: 'Given your actual map, after 10000 bursts of activity, how many bursts cause a node to become infected?',
+        answer: day_22,
         example: function () {
-            return [];
+            return `..#
+#..
+...`;
         },
     });
 }());
