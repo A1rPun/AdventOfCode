b86529ff1e51d0c07ed3f67205060b48b9bf62c7
Thom Bakker <thombakker@gmail.com>
2018-12-01 16:09:49 +0100

    :bug: 2017 Day 25 WIP

--- 2017/day_25/day_25.js
+++ 2017/day_25/day_25.js
@@ -1,18 +1,36 @@
 (function () {
-    function day_(puzzle) {
+    function day_25(puzzle) {
         return Promise.resolve();
     }
     December.addDay({
-        //day: ,
+        day: 25,
         year: 2017,
-        title: '',
-        questions: '',
-        answer: day_,
-        input: function () {
-            return [];
-        },
+        title: 'The Halting Problem',
+        questions: 'What is the diagnostic checksum it produces once it\'s working again?',
+        answer: day_25,
         example: function () {
-            return [];
+            return `Begin in state A.
+Perform a diagnostic checksum after 6 steps.
+
+In state A:
+    If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state B.
+    If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state B.
+
+In state B:
+    If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state A.
+    If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state A.`;
         },
     });
 }());
