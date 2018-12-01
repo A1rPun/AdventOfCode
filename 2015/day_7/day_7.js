c70ddced75fc8d07c50eb47ee261abcd6f49c6bd
Thom Bakker <thombakker@gmail.com>
2018-12-01 16:09:18 +0100

    :bug: 2015 day 7 WIP

--- 2015/day_7/day_7.js
+++ 2015/day_7/day_7.js
@@ -1,18 +1,57 @@
 (function () {
-    function day_(puzzle) {
-        return Promise.resolve();
+    function bitwise(lookup, x, operator, y) {
+        switch (operator.toUpperCase()) {
+            case 'AND':
+                return lookup[x] & lookup[y];
+            case 'OR':
+                return lookup[x] | lookup[y];
+            case 'LSHIFT':
+                return lookup[x] << +y;
+            case 'RSHIFT':
+                return lookup[x] >> +y;
+        }
+    }
+
+    function day_7(puzzle) {
+        const maxLength = 65535;
+        const instructions = puzzle.split('\n');
+        const wires = {};
+        for (let i = 0; i < instructions.length; i++) {
+            let instruction = instructions[i].split(' -> ');
+            const wire = instruction.pop();
+            instruction = instruction[0].split(' ');
+            let signal;
+            switch (instruction.length) {
+                case 2:
+                    signal = maxLength - wires[instruction[1]];
+                    break;
+                case 3:
+                    signal = bitwise(wires, ...instruction);
+                    break;
+                default:
+                    signal = +instruction[0];
+                    break;
+            }
+            wires[wire] = signal;
+        }
+        return Promise.resolve([wires]);
     }
     December.addDay({
-        //day: ,
+        day: 7,
         year: 2015,
-        title: '',
-        questions: '',
-        answer: day_,
-        input: function () {
-            return [];
-        },
+        title: 'Some Assembly Required',
+        questions: 'In little Bobby\'s kit\'s instructions booklet, what signal is ultimately provided to wire a?',
+        answer: day_7,
         example: function () {
-            return [];
+            return `123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i`;
         },
+        development: true,
     });
 }());
