60bcfe6ae7d9e96ee186d4749e37dff791db7cde
Thom Bakker <thombakker@gmail.com>
2018-12-02 13:20:27 +0100

    :star: 2018 Day 2 Complete!

--- 2018/day_2/day_2.js
+++ 2018/day_2/day_2.js
@@ -1,18 +1,54 @@
 (function () {
-    function day_(puzzle) {
-        return Promise.resolve();
+    function day_2(puzzle) {
+        puzzle = puzzle.split('\n');
+        const boxIds = puzzle.map(x => x.split('').reduce((acc, curr) => {
+            if (!acc[curr])
+                acc[curr] = 0;
+            acc[curr]++;
+            return acc;
+        }, {}));
+        const letterCount = boxIds.reduce((acc, curr) => {
+            const values = Object.values(curr);
+            if (~values.indexOf(2)) acc[0]++;
+            if (~values.indexOf(3)) acc[1]++;
+            return acc;
+        }, [0, 0]);
+        const answer1 = letterCount.reduce((a, b) => a * b);
+
+        let answer2;
+        for (let i = 0; i < puzzle.length; i++) {
+            const first = puzzle[i];
+            for (let j = 0; j < puzzle.length; j++) {
+                const second = puzzle[j];
+                if (first === second) continue;
+                let sameChars = '';
+                for (let k = 0; k < first.length; k++) {
+                    if (first[k] === second[k])
+                        sameChars += first[k];
+                }
+                if (sameChars.length === first.length - 1) {
+                    answer2 = sameChars;
+                    break;
+                }
+            }
+        }
+
+        return Promise.resolve([answer1, answer2]);
     }
     December.addDay({
-        //day: ,
+        day: 2,
         year: 2018,
-        title: '',
-        questions: '',
-        answer: day_,
-        input: function () {
-            return [];
-        },
+        title: 'Inventory Management System',
+        questions: 'What is the checksum for your list of box IDs?',
+        answer: day_2,
         example: function () {
-            return [];
+            return `abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz`;
         },
     });
 }());
