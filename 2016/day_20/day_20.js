a53f1efe055ceb9b945e06e5d1839bfd562a1a8e
Thom Bakker <thombakker@gmail.com>
2018-12-04 17:34:26 +0100

    :star: 2016 Day 20 complete!

--- 2016/day_20/day_20.js
+++ 2016/day_20/day_20.js
@@ -1,39 +1,42 @@
 (function () {
     function day_20(puzzle) {
-        var whitelist = [];
-        puzzle.push('4294967296-4294967297'); // MAX 4294967295
-        puzzle = puzzle
-            .map(function (x) {
-                var split = x.split('-');
-                return { low: +split[0], high: +split[1] };
-            })
-            .sort(function (a, b) { return a.low - b.low; });
+        puzzle = puzzle.split('\n');
+        puzzle.push(puzzle.length > 10 ? '4294967296-4294967296' : '10-10'); // MAX 4294967295
+        puzzle = puzzle.map(x => {
+            const [low, high] = x.split('-').map(x => parseInt(x, 10));
+            return { low, high };
+        }).sort((a, b) => a.low - b.low);
 
-        for (var i = 0, l = puzzle.length - 1; i < l; i++) {
-            var item1 = puzzle[i];
-            var item2 = puzzle[i + 1];
-            var c = item1.high + 1;
-            if (c < item2.low && item1.low < item2.low) {
-                whitelist.push({ start: c, count: item2.low - c });
-            } else {
-                item2.low = c;
-            }
-        }
-        var count = whitelist.reduce(function (a, b) { return a + b.count; }, 0);
-        return Promise.resolve([whitelist[0].start, count, whitelist]);
+        const whitelist = {};
+        let currentHighest = 0;
+        puzzle.forEach(x => {
+            const current = x.high + 1;
+
+            if (x.low > currentHighest)
+                whitelist[currentHighest] = x.low - currentHighest;
+
+            if (current > currentHighest)
+                currentHighest = current;
+        });
+
+        const answer1 = Math.min(...Object.keys(whitelist));
+        const answer2 = Object.values(whitelist).reduce((a, b) => a + b);
+        return Promise.resolve([answer1, answer2]);
     }
 
     December.addDay({
         day: 20,
         year: 2016,
         title: 'Firewall Rules',
-        questions: ['What is the lowest-valued IP that is not blocked?', 'How many IPs are allowed by the blacklist?'],
+        questions: [
+            'What is the lowest-valued IP that is not blocked?',
+            'How many IPs are allowed by the blacklist?'
+        ],
         answer: day_20,
         example: function () {
             return `5-8
 0-2
 4-7`;
         },
-        development: true,
     });
 }());
