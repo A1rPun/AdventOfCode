6f78bbd4c7c945a817b27642a2f4a5cc15c6eccb
Thom Bakker <thombakker@gmail.com>
2018-12-08 10:03:48 +0100

    :cyclone: Added some resources

--- 2016/day_22/day_22.js
+++ 2016/day_22/day_22.js
@@ -1,22 +1,21 @@
 (function () {
-  function day_22(puzzle) {
-    const nodes = puzzle.map(x => x.match(/\d+/g).map(d => parseInt(d, 10)));
-    const pairs = nodes.reduce((acc, node) => {
-      const pair = node[3] ? nodes.filter(x => x !== node && x[4] < node[3]) : [];
-      if (pair.length)
-        acc.push([node, pair[0]]);
-      return acc;
-    }, []);
-    const answer1 = pairs.length;
-    return Promise.resolve(answer1);
-  }
+    function day_22(puzzle) {
+        const nodes = puzzle.split('\n').map(x => x.match(/\d+/g).map(d => parseInt(d, 10)));
+        const pairs = nodes.reduce((acc, node) => {
+            const pair = node[3] ? nodes.filter(x => x !== node && x[4] < node[3]) : [];
+            if (pair.length)
+                acc.push([node, pair[0]]);
+            return acc;
+        }, []);
+        const answer1 = pairs.length;
+        return Promise.resolve(answer1);
+    }
 
     December.addDay({
         day: 22,
         year: 2016,
         title: 'Grid Computing',
-        questions: '',
+        questions: 'How many viable pairs of nodes are there?',
         answer: day_22,
-        example: function () { return ''; }
     });
 }());
