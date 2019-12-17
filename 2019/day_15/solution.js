fe9e4f90bbb8969bdb4b957d70d522fd5bdd83a4
A1rPun <thombakker@gmail.com>
2019-12-17 22:23:28 +0100

    :construction: 2019 Day 15 WIP

--- 2019/day_15/solution.js
+++ 2019/day_15/solution.js
@@ -1,18 +1,49 @@
 (function() {
+  const directions = {
+    north: 1,
+    south: 2,
+    west: 3,
+    east: 4,
+  };
+
+  const statusCodes = {
+    wall: 0,
+    requested: 1,
+    oxygen: 2,
+  };
+
+  const tiles = {
+    empty: ' ',
+    droid: 'D',
+    walls: '#',
+    visited: '.',
+  };
+
+  function answer1(memory) {
+    const computer = new December.IntCode(memory);
+    const width = 50;
+    const height = 50;
+    const canvas = Array.from(Array(height), () =>
+      Array.from(Array(width), () => tiles.empty)
+    );
+    let movement = directions.north;
+    // while (!computer.halted) {
+    //   const [status] = computer.run(movement, 1);
+    // }
+    return Promise.resolve();
+  }
+
   December.addDay({
-    // day: 15,
+    day: 15,
     year: 2019,
     title: 'Oxygen System',
-    questions: ['', ''],
-    answer1: puzzle => Promise.resolve(),
-    answer2: puzzle => Promise.resolve(),
-    example: [
-      // {
-      //   input: 123456789,
-      //   solutions: [],
-      //   answer: 1,
-      // },
+    questions: [
+      'What is the fewest number of movement commands required to move the repair droid from its starting position to the location of the oxygen system?',
+      '',
     ],
+    answer1,
+    answer2: puzzle => Promise.resolve(),
+    example: [],
     solutions: [],
   });
 })();
