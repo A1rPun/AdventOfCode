d8f05c05cee75cc708d4160b4268f586c96d4e8d
A1rPun <thombakker@gmail.com>
2019-12-19 22:15:00 +0100

    :star: 2019 Day 19 complete

--- 2019/day_19/solution.js
+++ 2019/day_19/solution.js
@@ -1,18 +1,48 @@
 (function() {
+  function checkVector(memory, x, y) {
+    const computer = new December.IntCode(memory);
+    computer.input = [x, y];
+    const [force] = computer.run();
+    return force;
+  }
+
+  function countBeam(memory, stride) {
+    const mapSize = stride * stride;
+    let result = 0;
+
+    for (let i = 0; i < mapSize; i++) {
+      const x = i % stride;
+      const y = Math.floor(i / stride);
+      result += checkVector(memory, x, y);
+    }
+    return result;
+  }
+
   December.addDay({
-    // day: ,
+    day: 19,
     year: 2019,
-    title: '',
-    questions: ['', ''],
-    answer1: puzzle => Promise.resolve(),
-    answer2: puzzle => Promise.resolve(),
-    example: [
-      // {
-      //   input: 123456789,
-      //   solutions: [],
-      //   answer: 1,
-      // },
+    title: 'Tractor Beam',
+    questions: [
+      'How many points are affected by the tractor beam in the 50x50 area closest to the emitter?',
+      "What value do you get if you take that point's X coordinate, multiply it by 10000, then add the point's Y coordinate?",
     ],
-    solutions: [],
+    answer1: memory => Promise.resolve(countBeam(memory, 50)),
+    answer2: memory => {
+      const houseSize = 100 - 1;
+      const pos = new Vector(houseSize * 5, houseSize * 10);
+      while (true) {
+        if (checkVector(memory, pos.x, pos.y)) {
+          if (checkVector(memory, pos.x + houseSize, pos.y - houseSize)) {
+            break;
+          }
+          pos.y++;
+        } else {
+          pos.x++;
+        }
+      }
+      return Promise.resolve(pos.x * 10000 + (pos.y - houseSize));
+    },
+    example: [],
+    solutions: [162, 13021056],
   });
 })();
