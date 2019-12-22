cdc22ad04ca4567f18103afc888d53394174d2a8
A1rPun <thombakker@gmail.com>
2019-12-22 16:21:26 +0100

    :star: 2019 Day 16 complete

--- 2019/day_16/solution.js
+++ 2019/day_16/solution.js
@@ -1,41 +1,44 @@
 (function() {
-  function genPattern(n, length) {
+  function genPattern(n) {
     let pattern = '0,1,0,-1'
       .split(',')
       .flatMap(x => (x + ',').repeat(n).split(','))
       .filter(x => x);
 
-    if (pattern.length < length) {
-      const times = Math.ceil(length / pattern.length);
-      const nt = (pattern.join(',') + ',').repeat(times);
-      pattern = nt.split(',');
-      pattern.pop();
-    }
     pattern.push(pattern.shift());
     return pattern;
   }
+  const genPatternCached = December.cache(genPattern);
 
-  function getSignal(input) {
+  function getMessage(input) {
     let output = input.split('');
 
     for (let i = 0; i < 100; i++) {
-      let phase = [];
+      let signal = [];
       for (let j = 0; j < output.length; j++) {
-        const pattern = genPattern(j + 1, output.length);
+        const pattern = genPatternCached(j + 1);
         let result = 0;
-        for (let k = 0; k < output.length; k++) {
-          result += pattern[k] * output[k];
+        // k can be j because of the leading zeroes of the pattern
+        for (let k = j; k < output.length; k++) {
+          result += pattern[k % pattern.length] * output[k];
         }
-        phase.push(
-          result
-            .toString()
-            .split('')
-            .pop()
-        );
+        signal.push(Math.abs(result) % 10);
       }
-      output = phase;
+      // December.log(signal.join(''));
+      output = signal;
     }
-    return output;
+    return output.slice(0, 8).join('');
+  }
+
+  function getMessageOptimized(input) {
+    let signal = input.split('').map(December.toInt);
+
+    for (let i = 0; i < 100; i++) {
+      for (let j = signal.length; j--; ) {
+        signal[j] = ((signal[j + 1] || 0) + signal[j]) % 10;
+      }
+    }
+    return signal.slice(0, 8).join('');
   }
 
   December.addDay({
@@ -46,13 +49,15 @@
       'After 100 phases of FFT, what are the first eight digits in the final output list?',
       'After repeating your input signal 10000 times and running 100 phases of FFT, what is the eight-digit message embedded in the final output list?',
     ],
-    answer1: puzzle => {
-      let output = getSignal(puzzle);
-      return Promise.resolve(output.slice(0, 8).join(''));
-    },
+    answer1: puzzle => Promise.resolve(getMessage(puzzle)),
     answer2: puzzle => {
-      let output = getSignal(puzzle);
-      return Promise.resolve(output.slice(0, 8).join(''));
+      const offset = December.toInt(puzzle.slice(0, 7));
+      const signal = getMessageOptimized(
+        puzzle
+          .repeat(10000)
+          .slice(offset)
+      );
+      return Promise.resolve(signal);
     },
     example: [
       {
@@ -70,7 +75,19 @@
         solutions: ['52432133'],
         answer: 1,
       },
+      {
+        input: '03036732577212944063491565474664',
+        solutions: ['24465799', '84462026'],
+      },
+      {
+        input: '02935109699940807407585447034323',
+        solutions: ['82441489', '78725270'],
+      },
+      {
+        input: '03081770884921959731165446850517',
+        solutions: ['52486276', '53553731'],
+      },
     ],
-    solutions: [88323090],
+    solutions: ['88323090', '50077964'],
   });
 })();
