a97bbf178a2e89015f9994e2b00f7f2c967ddce9
A1rPun <thombakker@gmail.com>
2019-12-23 01:11:14 +0100

    :star: 2019 Day 22 question 1

--- 2019/day_22/solution.js
+++ 2019/day_22/solution.js
@@ -4,10 +4,16 @@
     increment: (deck, n) => {
       let jump = 0;
       let result = [];
+
       for (let i = 0; i < deck.length; i++) {
         result[jump % deck.length] = deck[i];
         jump += n;
-        if (typeof result[jump % deck.length] === 'undefined') jump++;
+        if (
+          jump >= deck.length &&
+          typeof result[jump % deck.length] !== 'undefined'
+        ) {
+          jump++;
+        }
       }
       return [...result];
     },
@@ -16,50 +22,61 @@
     },
   };
 
+  function shuffleDeck(deck, puzzle) {
+    const instructions = puzzle.split('\n');
+
+    instructions.forEach(instruction => {
+      const parts = instruction.split(' ');
+      if (parts[0] === 'cut') {
+        deck = shuffles.cut(deck, +parts[1]);
+      } else if (parts[0] === 'deal' && parts[2] === 'increment') {
+        deck = shuffles.increment(deck, +parts[3]);
+      } else if (parts[0] === 'deal' && parts[2] === 'new') {
+        deck = shuffles.new(deck);
+      } else {
+        throw new Error('Invalid argument');
+      }
+    });
+    return deck;
+  }
+
   December.addDay({
     day: 22,
     year: 2019,
     title: 'Slam Shuffle',
     questions: [
       'After shuffling your factory order deck of 10007 cards, what is the position of card 2019?',
-      '',
+      'After shuffling your new, giant, factory order deck that many times, what number is on the card that ends up in position 2020?',
     ],
-    answer1: puzzle => {
-      let deck = December.range(10007);
-      const instructions = puzzle.split('\n');
-      instructions.forEach(instruction => {
-        const parts = instruction.split(' ');
-        if (parts[0] === 'cut') {
-          deck = shuffles.cut(deck, +parts[1]);
-        } else if (parts[0] === 'deal' && parts[2] === 'increment') {
-          deck = shuffles.increment(deck, +parts[3]);
-        } else if (parts[0] === 'deal' && parts[2] === 'new') {
-          deck = shuffles.new(deck);
-        } else {
-          throw new Error('Invalid argument');
-        }
-      });
-      return Promise.resolve([deck[2019], deck]);
-    },
-    answer2: puzzle => Promise.resolve(),
+    answer1: puzzle =>
+      Promise.resolve(
+        shuffleDeck(December.range(10007), puzzle).findIndex(x => x === 2019)
+      ),
+    answer2: puzzle =>
+      Promise.resolve(
+        // shuffleDeck(
+        //   December.range(119315717514047),
+        //   puzzle.repeat(101741582076661)
+        // )[2020]
+      ),
     example: [
       {
-        input: `deal with increment 4`,
-        solutions: [],
+        input: `deal with increment 7`,
+        solutions: [4126],
         answer: 1,
       },
       {
         input: `cut 6
 deal with increment 7
 deal into new stack`,
-        solutions: [],
+        solutions: [5922],
         answer: 1,
       },
       {
         input: `deal with increment 7
 deal with increment 9
 cut -2`,
-        solutions: [],
+        solutions: [7115],
         answer: 1,
       },
       {
@@ -73,10 +90,10 @@ cut 3
 deal with increment 9
 deal with increment 3
 cut -1`,
-        solutions: [],
+        solutions: [1219],
         answer: 1,
       },
     ],
-    solutions: [],
+    solutions: [5472],
   });
 })();
