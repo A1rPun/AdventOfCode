6f6d764fbbc6fe705b1e41d2658c73122c590414
A1rPun <thombakker@gmail.com>
2020-12-08 21:32:43 +0100

    :star: 2020 Day 4 question 1

--- 2020/day_4/solution.js
+++ 2020/day_4/solution.js
@@ -1,11 +1,27 @@
 (function() {
+  const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+  const isValidPassport = passport =>
+    requiredFields.every(field => passport.includes(field));
+  const getValidPassports = passports => passports.filter(isValidPassport);
+  const getReallyValidPassports = passports =>
+    getValidPassports(passports).filter(isReallyValidPassport);
+
+  function isReallyValidPassport(passport) {
+    
+  }
+
   December.addDay({
     day: 4,
     year: 2020,
     title: 'Passport Processing',
-    questions: ['In your batch file, how many passports are valid?', ''],
-    answer1: puzzle => Promise.resolve(),
-    answer2: puzzle => Promise.resolve(),
+    questions: [
+      'In your batch file, how many passports are valid?',
+      'In your batch file, how many passports are valid?',
+    ],
+    answer1: puzzle =>
+      Promise.resolve(getValidPassports(puzzle.split('\n\n')).length),
+    answer2: puzzle =>
+      Promise.resolve(getReallyValidPassports(puzzle.split('\n\n')).length),
     example: [
       {
         input: `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
@@ -24,7 +40,37 @@ iyr:2011 ecl:brn hgt:59in`,
         solutions: [2],
         answer: 1,
       },
+      {
+        input: `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`,
+        solutions: [8, 4],
+        answer: 2,
+      },
     ],
-    solutions: [],
+    solutions: [216],
   });
 })();
