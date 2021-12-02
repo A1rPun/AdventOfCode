155901aaef9a6c8611cc5e942b2f8f395d22b0d4
A1rPun <thombakker@gmail.com>
2021-12-02 19:23:51 +0100

    :bug: Module

--- 2021/module.js
+++ 2021/module.js
@@ -1,7 +1,53 @@
 import day1 from './day_1/solution.js';
 import day2 from './day_2/solution.js';
+import day3 from './day_3/solution.js';
+import day4 from './day_4/solution.js';
+import day5 from './day_5/solution.js';
+import day6 from './day_6/solution.js';
+import day7 from './day_7/solution.js';
+import day8 from './day_8/solution.js';
+import day9 from './day_9/solution.js';
+import day10 from './day_10/solution.js';
+import day11 from './day_11/solution.js';
+import day12 from './day_12/solution.js';
+import day13 from './day_13/solution.js';
+import day14 from './day_14/solution.js';
+import day15 from './day_15/solution.js';
+import day16 from './day_16/solution.js';
+import day17 from './day_17/solution.js';
+import day18 from './day_18/solution.js';
+import day19 from './day_19/solution.js';
+import day20 from './day_20/solution.js';
+import day21 from './day_21/solution.js';
+import day22 from './day_22/solution.js';
+import day23 from './day_23/solution.js';
+import day24 from './day_24/solution.js';
+import day25 from './day_25/solution.js';
 
 export default [
   day1,
-  day2
+  day2,
+  day3,
+  day4,
+  day5,
+  day6,
+  day7,
+  day8,
+  day9,
+  day10,
+  day11,
+  day12,
+  day13,
+  day14,
+  day15,
+  day16,
+  day17,
+  day18,
+  day19,
+  day20,
+  day21,
+  day22,
+  day23,
+  day24,
+  day25,
 ];
