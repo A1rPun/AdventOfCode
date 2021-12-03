ad54bee2ec020eacc3fd2120969229f4863938d2
A1rPun <thombakker@gmail.com>
2021-12-03 20:55:44 +0100

    :bug: More module

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-﻿import December from './december.js';
+import December from './december.js';
 import perfTimer from './perfTimer.js';
+import years from '../module.js';
 
-import days2015 from '../2015/module.js';
-import days2016 from '../2016/module.js';
-import days2017 from '../2017/module.js';
-import days2018 from '../2018/module.js';
-import days2019 from '../2019/module.js';
-import days2020 from '../2020/module.js';
-import days2021 from '../2021/module.js';
-
-[days2015, days2016, days2017, days2018, days2019, days2020, days2021].forEach(
-  (y, i) =>
+years.forEach((y, i) =>
     y.forEach((x, j) =>
       December.addDay({
         ...x,
---
+++ module.js
@@ -0,0 +1,17 @@
+import days2015 from './2015/module.js';
+import days2016 from './2016/module.js';
+import days2017 from './2017/module.js';
+import days2018 from './2018/module.js';
+import days2019 from './2019/module.js';
+import days2020 from './2020/module.js';
+import days2021 from './2021/module.js';
+
+export default [
+  days2015,
+  days2016,
+  days2017,
+  days2018,
+  days2019,
+  days2020,
+  days2021,
+];
