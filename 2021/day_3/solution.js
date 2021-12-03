345a529583c3308058bb8712704e75fac32492df
A1rPun <thombakker@gmail.com>
2021-12-03 20:50:08 +0100

    :star: Day 3 Complete

--- 2021/day_3/solution.js
+++ 2021/day_3/solution.js
@@ -1,29 +1,41 @@
-// const dectoBin = (dec) => Object.values(dec).map(x => x < half ? '1' : '0').join('');
+const mostCommon = (x, half) => (x >= half ? '1' : '0');
+const leastCommon = (x, half) => (x < half ? '1' : '0');
+const countRow = (lines, column) =>
+  lines.reduce((acc, cur) => acc + +(cur[column] === '1'), 0);
 
 export default {
-  title: 'Test',
-  questions: ['a', 'b'],
+  title: 'Binary Diagnostic',
+  questions: [
+    'What is the power consumption of the submarine?',
+    'What is the life support rating of the submarine?W',
+  ],
   answer1: (puzzle) => {
-    const lines = puzzle.split('\n')
-    const abc = lines.reduce((acc, cur) => {
-      const idx = cur.split('');
-      idx.forEach((x, i) => {
-        if (!acc[i]) acc[i] = 0;
-        if (x === '1') acc[i]++;
-      });
-      return acc;
-    }, {});
-
-    const half = Math.floor(lines.length / 2);
-    const gamma = Object.values(abc).map(x => x > half ? '1' : '0').join('');
-    const epsilon = Object.values(abc).map(x => x < half ? '1' : '0').join('');
-
-    console.log(half, gamma, epsilon);
-    console.log(abc);
+    const lines = puzzle.split('\n');
+    const iter = { length: lines[0].length };
+    const counts = Array.from(iter, (_, i) => countRow(lines, i));
+    const half = lines.length / 2;
+    const gamma = counts.map((x) => mostCommon(x, half)).join('');
+    const epsilon = counts.map((x) => leastCommon(x, half)).join('');
 
     return parseInt(gamma, 2) * parseInt(epsilon, 2);
   },
-  answer2: (puzzle) => { },
+  answer2: (puzzle) => {
+    const filterLines = (lines, predicate, column = 0) => {
+      const count = countRow(lines, column);
+      const result = predicate(count, lines.length / 2);
+      const filtered = lines.filter((x) => x[column] === result);
+
+      return filtered.length > 1
+        ? filterLines(filtered, predicate, column + 1)
+        : filtered;
+    };
+
+    const lines = puzzle.split('\n');
+    const [OGeneratorRating] = filterLines(lines, mostCommon);
+    const [Co2ScrubberRating] = filterLines(lines, leastCommon);
+
+    return parseInt(OGeneratorRating, 2) * parseInt(Co2ScrubberRating, 2);
+  },
   example: [
     {
       input: `00100
@@ -38,8 +50,8 @@ export default {
 11001
 00010
 01010`,
-      solutions: [198],
+      solutions: [198, 230],
     },
   ],
-  solutions: [],
+  solutions: [852500, 1007985],
 };
