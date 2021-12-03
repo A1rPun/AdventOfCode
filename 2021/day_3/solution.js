9edb4d9f15cd717bb751931be4296561fe8b6ff1
Thom Bakker <t.bakker@qwoater.nl>
2021-12-03 18:00:11 +0100

    :pencil: 2021 Day 3 WIP

--- 2021/day_3/solution.js
+++ 2021/day_3/solution.js
@@ -1,14 +1,45 @@
+// const dectoBin = (dec) => Object.values(dec).map(x => x < half ? '1' : '0').join('');
+
 export default {
-  title: '',
-  questions: ['', ''],
-  answer1: (puzzle) => {},
-  answer2: (puzzle) => {},
+  title: 'Test',
+  questions: ['a', 'b'],
+  answer1: (puzzle) => {
+    const lines = puzzle.split('\n')
+    const abc = lines.reduce((acc, cur) => {
+      const idx = cur.split('');
+      idx.forEach((x, i) => {
+        if (!acc[i]) acc[i] = 0;
+        if (x === '1') acc[i]++;
+      });
+      return acc;
+    }, {});
+
+    const half = Math.floor(lines.length / 2);
+    const gamma = Object.values(abc).map(x => x > half ? '1' : '0').join('');
+    const epsilon = Object.values(abc).map(x => x < half ? '1' : '0').join('');
+
+    console.log(half, gamma, epsilon);
+    console.log(abc);
+
+    return parseInt(gamma, 2) * parseInt(epsilon, 2);
+  },
+  answer2: (puzzle) => { },
   example: [
-    // {
-    //   input: 123456789,
-    //   solutions: [],
-    //   answer: 1,
-    // },
+    {
+      input: `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`,
+      solutions: [198],
+    },
   ],
   solutions: [],
 };
