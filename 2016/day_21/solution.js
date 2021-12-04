9875a87066fab09aef5d50ea9f1c58450290cdb6
A1rPun <thombakker@gmail.com>
2021-12-04 16:56:53 +0100

    :sparkles: 2016 Improve Day 21

--- 2016/day_21/solution.js
+++ 2016/day_21/solution.js
@@ -1,34 +1,35 @@
 import { rotate, log } from '../../js/december.js';
 
-var actions = {
-  swap: function(charArr, split) {
+const actions = {
+  swap: function (charArr, split) {
+    let result = [...charArr];
+
     if (split[1] === 'position') {
-      charArr[split[2]] = [
-        charArr[split[5]],
-        (charArr[split[5]] = charArr[split[2]]),
+      result[split[2]] = [
+        result[split[5]],
+        (result[split[5]] = result[split[2]]),
       ][0];
     } else {
-      var x = charArr.indexOf(split[2]);
-      var y = charArr.indexOf(split[5]);
-      charArr[x] = [charArr[y], (charArr[y] = charArr[x])][0];
+      var x = result.indexOf(split[2]);
+      var y = result.indexOf(split[5]);
+      result[x] = [result[y], (result[y] = result[x])][0];
     }
-    return charArr;
+    return result;
   },
-  rotate: function(charArr, split) {
+  rotate: function (charArr, split) {
+    let result;
+
     if (split[1] === 'left') {
-      charArr = rotate(charArr, split[2]);
+      result = rotate(charArr, split[2]);
     } else if (split[1] === 'right') {
-      charArr = rotate(charArr, -split[2]);
+      result = rotate(charArr, -split[2]);
     } else {
       var x = charArr.indexOf(split[6]);
-      charArr = rotate(
-        charArr,
-        -(1 + x + (+x > 3 ? 1 : 0)) % charArr.length
-      );
+      result = rotate(charArr, -(1 + x + (+x > 3 ? 1 : 0)) % charArr.length);
     }
-    return charArr;
+    return result;
   },
-  reverse: function(charArr, split) {
+  reverse: function (charArr, split) {
     var x = +split[2];
     var y = +split[4] + 1;
     var a = charArr.slice(0, x);
@@ -36,11 +37,12 @@ var actions = {
     var c = charArr.slice(y, charArr.length);
     return a.concat(b).concat(c);
   },
-  move: function(charArr, split) {
-    var x = charArr[split[2]];
-    charArr.splice(split[2], 1);
-    charArr.splice(split[5], 0, x);
-    return charArr;
+  move: function (charArr, split) {
+    let result = [...charArr];
+    const x = result[split[2]];
+    result.splice(split[2], 1);
+    result.splice(split[5], 0, x);
+    return result;
   },
 };
 
@@ -50,24 +52,28 @@ function processInput(input, charArr) {
 }
 
 function day_21(puzzle, animate) {
-  puzzle = puzzle.split('\n');
-  var answer1 = 'abcdefgh';
-  return new Promise(function(resolve) {
+  const [start, input] = puzzle.split('\n\n');
+  const lines = input.split('\n');
+  let answer1 = start;
+
+  return new Promise(function (resolve) {
+    let i = 0;
+
     if (animate) {
-      var interval = 100;
-      var i = 0;
-      var fn = function() {
-        answer1 = processInput(puzzle[i], answer1);
+      const interval = 100;
+      const fn = () => {
+        answer1 = processInput(lines[i], answer1);
         i++;
-        if (i < puzzle.length) {
+        if (i < lines.length) {
           log(answer1.join(''), true);
           setTimeout(fn, interval);
         } else resolve(answer1.join(''));
       };
       setTimeout(fn, interval);
     } else {
-      for (var i = 0; i < puzzle.length; i++)
-        answer1 = processInput(puzzle[i], answer1);
+      for (; i < lines.length; i++) {
+        answer1 = processInput(lines[i], answer1);
+      }
       resolve(answer1.join(''));
     }
   });
@@ -79,9 +85,13 @@ export default {
     'Now, you just need to generate a new scrambled password and you can access the system. What is the result of scrambling abcdefgh?',
     'What is the un-scrambled version of the scrambled password fbgdceah?',
   ],
-  answer: day_21,
+  answer1: day_21,
+  answer2: (puzzle) => '',
   example: [
-    `swap position 4 with position 0
+    {
+      input: `abcde
+
+swap position 4 with position 0
 swap letter d with letter b
 reverse positions 0 through 4
 rotate left 1 step
@@ -89,7 +99,13 @@ move position 1 to position 4
 move position 3 to position 0
 rotate based on position of letter b
 rotate based on position of letter d`,
+      solutions: ['decab'],
+    },
+    {
+      input: 'fbgdceah',
+      solutions: [, ''],
+    }
   ],
-  exampleSolutions: ['decab'],
   solutions: ['agcebfdh'],
+  hasAnimation: true,
 };
