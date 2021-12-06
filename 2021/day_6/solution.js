d16be084cce81b0a78e5918b76aab9bce5fdfb6c
A1rPun <thombakker@gmail.com>
2021-12-06 12:45:54 +0100

    :star: 2021 Day 5 part 1

--- 2021/day_6/solution.js
+++ 2021/day_6/solution.js
@@ -1,13 +1,45 @@
+class Fish {
+  constructor(days = 8) {
+    this.days = days;
+  }
+  tick(){
+    this.days--;
+    if(this.days < 0){
+      this.days =6;
+      return true;
+    }
+  }
+}
+
 export default {
-  title: '',
-  questions: ['', ''],
-  answer1: (puzzle) => {},
+  title: 'Lanternfish',
+  questions: [
+    'How many lanternfish would there be after 80 days?',
+    ''
+  ],
+  answer1: (puzzle) => {
+    let school = puzzle
+      .split(',')
+      .map(x => new Fish(x));
+
+    for(let i = 0; i < 80; i++){
+      const newFish = [];
+      school.forEach(x =>{ 
+        if(x.tick()){
+          newFish.push(new Fish());
+        }
+      });
+      school = [...school, ...newFish];
+    }
+    return school.length;
+  },
   answer2: (puzzle) => {},
   example: [
-    // {
-    //   input: 123456789,
-    //   solutions: [],
-    // },
+    {
+      input: '3,4,3,1,2',
+      solutions: [5934],
+    },
   ],
   solutions: [],
 };
+
