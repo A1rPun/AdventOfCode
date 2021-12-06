ebfcd08918ace0a27f9562d6b565ee862591a333
Thom Bakker <t.bakker@qwoater.nl>
2021-12-06 18:15:09 +0100

    :bug: Fix day 6

--- 2021/day_6/solution.js
+++ 2021/day_6/solution.js
@@ -1,13 +1,12 @@
-class Fish {
-  constructor(days = 8) {
-    this.days = days;
-  }
-  tick(){
-    this.days--;
-    if(this.days < 0){
-      this.days =6;
-      return true;
-    }
+import { sum } from '../../js/december.js';
+
+function func(lifeTime, time, max = 80) {
+  if (time === max) {
+    return 1;
+  } else if (lifeTime > 0) {
+    return func(lifeTime - 1, time + 1, max)
+  } else {
+    return func(6, time + 1, max) + func(8, time + 1, max);
   }
 }
 
@@ -15,31 +14,15 @@ export default {
   title: 'Lanternfish',
   questions: [
     'How many lanternfish would there be after 80 days?',
-    ''
+    'How many lanternfish would there be after 256 days?'
   ],
-  answer1: (puzzle) => {
-    let school = puzzle
-      .split(',')
-      .map(x => new Fish(x));
-
-    for(let i = 0; i < 80; i++){
-      const newFish = [];
-      school.forEach(x =>{ 
-        if(x.tick()){
-          newFish.push(new Fish());
-        }
-      });
-      school = [...school, ...newFish];
-    }
-    return school.length;
-  },
-  answer2: (puzzle) => {},
+  answer1: (puzzle) =>  puzzle.split(',').map((x) => func(x, 0, 80)).reduce(sum),
+  answer2: (puzzle) => puzzle.split(',').map((x) => func(x, 0, 200)).reduce(sum),
   example: [
     {
       input: '3,4,3,1,2',
-      solutions: [5934],
+      solutions: [5934, 26984457539],
     },
   ],
-  solutions: [],
+  solutions: [390011,],
 };
-
