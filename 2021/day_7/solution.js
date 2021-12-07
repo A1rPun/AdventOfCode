14ff842baa5bc3f9891e0b5faec06960c927e273
Thom Bakker <t.bakker@qwoater.nl>
2021-12-07 19:07:39 +0100

    :star: 2021 Day 7 complete

--- 2021/day_7/solution.js
+++ 2021/day_7/solution.js
@@ -1,32 +1,40 @@
 import { toInt, sum } from '../../js/december.js';
 
-function costFuel(crabs, n) {
-  return crabs.map(x => Math.abs(x - n)).reduce(sum);
+function costFuel(puzzle, fn) {
+  const crabs = puzzle.split(',').map(toInt);
+  const min = Math.min(...crabs);
+  const max = Math.max(...crabs);
+  const fuels = [];
+
+  for (let i = min; i < max; i++) {
+    const total = crabs.map(x => fn(x, i)).reduce(sum);
+    fuels.push(total);
+  }
+  return Math.min(...fuels);
+}
+
+function simpleFuel(x, n) {
+  return Math.abs(x - n);
+}
+
+function betterFuel(x, n) {
+  const diff = Math.abs(x - n);
+  return (diff + 1) * (diff / 2);
 }
 
 export default {
-  title: `The Treachery of Whale's`,
+  title: `The Treachery of Whales`,
   questions: [
-    'How much fuel must they spend to align to that position?', 
-    ''
+    'How much fuel must they spend to align to that position?',
+    'How much fuel must they spend to align to that position?'
   ],
-  answer1: (puzzle) => {
-    const crabs = puzzle.split(',').map(toInt);
-    const min = Math.min(...crabs);
-    const max = Math.max(...crabs);
-    const fuels = [];
-
-    for (let i = min; i < max; i++) {
-      fuels.push(costFuel(crabs, i));
-    }
-    return Math.min(...fuels);
-  },
-  answer2: (puzzle) => {},
+  answer1: (puzzle) => costFuel(puzzle, simpleFuel),
+  answer2: (puzzle) => costFuel(puzzle, betterFuel),
   example: [
     {
       input: '16,1,2,0,4,2,7,1,2,14',
-      solutions: [37],
+      solutions: [37, 168],
     },
   ],
-  solutions: [],
+  solutions: [347011, 98363777],
 };
