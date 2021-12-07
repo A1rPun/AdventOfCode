77c1131c95f2b695621be7da165738d202629739
A1rPun <thombakker@gmail.com>
2021-12-07 07:37:36 +0100

    :star: 2021 Day 6 complete

--- 2021/day_6/solution.js
+++ 2021/day_6/solution.js
@@ -1,28 +1,200 @@
-import { sum } from '../../js/december.js';
+import { toInt, sum, safeAdd } from '../../js/december.js';
 
-function func(lifeTime, time, max = 80) {
-  if (time === max) {
-    return 1;
-  } else if (lifeTime > 0) {
-    return func(lifeTime - 1, time + 1, max)
-  } else {
-    return func(6, time + 1, max) + func(8, time + 1, max);
+function countFish(fishes, max) {
+  const days = Array(9).fill(0);
+  fishes.forEach((fish) => days[fish]++);
+
+  for (; max--; ) {
+    days.push(days.shift());
+    days[6] += days[8];
   }
+  return days.reduce(sum);
+}
+
+function parse(puzzle) {
+  return puzzle.split(',').map(toInt);
 }
 
 export default {
   title: 'Lanternfish',
   questions: [
     'How many lanternfish would there be after 80 days?',
-    'How many lanternfish would there be after 256 days?'
+    'How many lanternfish would there be after 256 days?',
   ],
-  answer1: (puzzle) =>  puzzle.split(',').map((x) => func(x, 0, 80)).reduce(sum),
-  answer2: (puzzle) => puzzle.split(',').map((x) => func(x, 0, 200)).reduce(sum),
+  answer1: (puzzle) => countFish(parse(puzzle), 80),
+  answer2: (puzzle) => countFish(parse(puzzle), 256),
   example: [
     {
       input: '3,4,3,1,2',
       solutions: [5934, 26984457539],
     },
+    {
+      input: '1,2,3,4,5',
+      solutions: [5730, 26135466306],
+    },
+    {
+      input: '1',
+      solutions: [1401, 6206821033],
+    },
   ],
-  solutions: [390011,],
+  solutions: [390011, 1746710169834],
 };
+
+/* /
+// Try 0, see git
+
+// Try 1
+function func(lifeTime, max, time = 0) {
+  if (time === max) {
+    return 1;
+  } else if (lifeTime > 0) {
+    return func(lifeTime - 1, max, time + 1);
+  }
+  return func(6, max, time + 1) + func(8, max, time + 1);
+}
+function countFish(fishes, max) {
+  return fishes.map((x) => func(x, max)).reduce(sum);
+}
+/* */
+
+/* /
+// Try 1.337
+const foo = (lifeTime, max) =>
+  !max ? 1 : lifeTime > 0
+    ? foo(lifeTime - 1, max - 1)
+    : foo(6, max - 1) + foo(8, max - 1);
+/* */
+
+/* /
+// Try 2
+// 18 days - 26 fishes
+
+// 1
+// - 2
+//      - 11
+//      - 18
+// - 9
+//      - 18
+// - 16
+
+// 2
+// - 3
+//      - 12
+// - 10
+// - 17
+
+// 3
+// - 4
+//      - 13
+// - 11
+// - 18
+
+// 3 ...
+
+// 4
+// - 5
+//      - 14
+// - 12
+
+function solve(lifeTime, max) {
+  let fishes = 1;
+
+  if (lifeTime < max) {
+    const length = Math.ceil((max - lifeTime) / 7);
+
+    for (let i = 0; i < length; i++) {
+      fishes += solve(lifeTime + 1 + i * 7, max - 8);
+    }
+  }
+  return fishes;
+}
+function countFish(fishes, max) {
+  return fishes.map((x) => solve(x, max)).reduce(sum);
+}
+/* */
+
+/* /
+// // Try 3
+// 0 1 2 3 4 5
+
+// 0 1 1 2 1 0
+
+// 0 1 1 2 1 0 0 0 0 0
+// 0 1 1 2 1 0 0 0 1 0 1
+// 0 1 1 2 1 0 0 0 1 1 1 1           day 18
+// 0 1 1 2 1 0 0 0 1 1 3 1 2           |
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1         |
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0       |
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 0     |
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 0 0   V
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 0 1
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 1 1
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 1 3
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 3 2
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 5 2 2
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 5 3 2 1
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 5 3 2 1 0
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 5 3 2 2 0 1
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 5 3 2 2 1 1 1
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 5 3 2 2 1 5 1 4
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 3 5 3 2 2 1 5 4 4 3
+
+// 0 1 1 2 1 0 0 0 1 1 3 2 2 1 0 1 1 4 === 21 + 5 === 26
+
+function countFish(fishes, max) {
+  const days = fishes.reduce((r, x) => safeAdd(r, x), {});
+
+  for (let i = 0; i < max; i++) {
+    const day = days[i];
+
+    if (day) {
+      safeAdd(days, i + 7, day);
+      safeAdd(days, i + 9, day);
+    }
+  }
+  return (
+    fishes.length +
+    Object.entries(days)
+      .filter(([key]) => key < max)
+      .map(([, val]) => val)
+      .reduce(sum)
+  );
+}
+/* */
+
+/* /
+// // Try 4
+// 0 1 2 3 4 5 6 7 8
+
+// 0 1 1 2 1 0 0 0 0
+
+// 1 1 2 1 0 0 0 0 0
+// 1 2 1 0 0 0 1 0 1
+// 2 1 0 0 0 1 1 1 1
+// 1 0 0 0 1 1 3 1 2
+// 0 0 0 1 1 3 2 2 1
+// 0 0 1 1 3 2 2 1 0
+// 0 1 1 3 2 2 1 0 0
+// 1 1 3 2 2 1 0 0 0
+// 1 3 2 2 1 0 1 0 1
+// 3 2 2 1 0 1 1 1 1
+// 2 2 1 0 1 1 4 1 3
+// 2 1 0 1 1 4 3 3 2
+// 1 0 1 1 4 3 5 2 2
+// 0 1 1 4 3 5 3 2 1
+// 1 1 4 3 5 3 2 1 0
+// 1 4 3 5 3 2 2 0 1
+// 4 3 5 3 2 2 1 1 1
+// 3 5 3 2 2 1 5 1 4 === 26
+
+function countFish(fishes, max) {
+  const days = Array(9).fill(0);
+  fishes.forEach((fish) => days[fish]++);
+
+  for (; max--; ) {
+    days.push(days.shift());
+    days[6] += days[8];
+  }
+  return days.reduce(sum);
+}
+/* */
