1810ad06f9d546734d5a705ce0bdadd00ee1b3cb
A1rPun <thombakker@gmail.com>
2021-12-07 11:43:07 +0100

    :star: 2021 Day 7 part 1

--- 2021/day_7/solution.js
+++ 2021/day_7/solution.js
@@ -1,13 +1,32 @@
+import { toInt, sum } from '../../js/december.js';
+
+function costFuel(crabs, n) {
+  return crabs.map(x => Math.abs(x - n)).reduce(sum);
+}
+
 export default {
-  title: '',
-  questions: ['', ''],
-  answer1: (puzzle) => {},
+  title: `The Treachery of Whale's`,
+  questions: [
+    'How much fuel must they spend to align to that position?', 
+    ''
+  ],
+  answer1: (puzzle) => {
+    const crabs = puzzle.split(',').map(toInt);
+    const min = Math.min(...crabs);
+    const max = Math.max(...crabs);
+    const fuels = [];
+
+    for (let i = min; i < max; i++) {
+      fuels.push(costFuel(crabs, i));
+    }
+    return Math.min(...fuels);
+  },
   answer2: (puzzle) => {},
   example: [
-    // {
-    //   input: 123456789,
-    //   solutions: [],
-    // },
+    {
+      input: '16,1,2,0,4,2,7,1,2,14',
+      solutions: [37],
+    },
   ],
   solutions: [],
 };
