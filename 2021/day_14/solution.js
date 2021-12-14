6bacb625c77ed949dd754f75944f2a3ca95e0eac
A1rPun <thombakker@gmail.com>
2021-12-14 19:51:01 +0100

    :sparkles: 2021 Day 14 Refactor

--- 2021/day_14/solution.js
+++ 2021/day_14/solution.js
@@ -2,79 +2,56 @@ import { safeAdd } from '../../js/december.js';
 
 function countOccurrence(str) {
   const dict = {};
-  for (let i = 0; i < str.length; i++)
-    safeAdd(dict, str[i]);
+  for (let i = 0; i < str.length; i++) safeAdd(dict, str[i]);
   return dict;
+}
+
+function parse(puzzle) {
+  const [axiom, abc] = puzzle.split('\n\n');
+  const pairs = abc.split('\n').reduce((acc, cur) => {
+    const [pair, insert] = cur.split(' -> ');
+    acc[pair] = insert;
+    return acc;
+  }, {});
+  return [axiom, pairs];
+}
+
+function insertPairs(pairs, polymer) {
+  let newstr = '';
+  for (let j = 0; j < polymer.length; j++) {
+    newstr += polymer[j];
+
+    const pair = `${polymer[j]}${polymer[j + 1]}`;
+
+    if (pairs[pair]) newstr += `${pairs[pair]}`;
+    else if (polymer[j + 1]) newstr += polymer[j + 1];
+  }
+  return newstr;
+}
+
+function solve(puzzle, num = 10) {
+  let [polymer, pairs] = parse(puzzle);
+
+  for (let i = 0; i < num; i++) {
+    polymer = insertPairs(pairs, polymer);
+  }
+  const counts = countOccurrence(polymer);
+
+  const values = Object.values(counts);
+  const max = Math.max(...values);
+  const min = Math.min(...values);
+
+  return max - min;
 }
 
 export default {
   title: 'Extended Polymerization',
   questions: [
     'What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?',
-    '',
+    'What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?',
   ],
-  answer1: (puzzle) => {
-    const [axiom, pairs] = puzzle.split('\n\n');
-    const dict = pairs.split('\n').reduce((acc, cur) => {
-      const [pair, insert] = cur.split(' -> ')
-      acc[pair] = insert;
-      return acc;
-    }, {});
-
-    let polymer = axiom;
-
-    for (let i = 0; i < 10; i++) {
-      let newstr = '';
-      for (let j = 0; j < polymer.length; j++) {
-        newstr += polymer[j];
-
-        const pair = `${polymer[j]}${polymer[j+1]}`;
-        
-        if (dict[pair])
-          newstr += `${dict[pair]}`;
-        else if (polymer[j + 1]) {
-          newstr += polymer[j + 1];
-        }
-      }
-      polymer = newstr;
-    }   
-
-
-    const counts = countOccurrence(polymer);
-
-    return counts;
-  },
-  answer2: (puzzle) => {
-    const [axiom, pairs] = puzzle.split('\n\n');
-    const dict = pairs.split('\n').reduce((acc, cur) => {
-      const [pair, insert] = cur.split(' -> ')
-      acc[pair] = insert;
-      return acc;
-    }, {});
-
-    let polymer = axiom;
-
-    for (let i = 0; i < 40; i++) {
-      let newstr = '';
-      for (let j = 0; j < polymer.length; j++) {
-        newstr += polymer[j];
-
-        const pair = `${polymer[j]}${polymer[j+1]}`;
-        
-        if (dict[pair])
-          newstr += `${dict[pair]}`;
-        else if (polymer[j + 1]) {
-          newstr += polymer[j + 1];
-        }
-      }
-      polymer = newstr;
-    }   
-
-
-    const counts = countOccurrence(polymer);
-
-    return counts;
-  },
+  answer1: (puzzle) => solve(puzzle),
+  answer2: (puzzle) => solve(puzzle, 40),
   example: [
     {
       input: `NNCB
@@ -95,8 +72,8 @@ BB -> N
 BC -> B
 CC -> N
 CN -> C`,
-      solutions: [1588],
+      solutions: [1588, 2188189693529],
     },
   ],
-  solutions: [],
+  solutions: [2891],
 };
