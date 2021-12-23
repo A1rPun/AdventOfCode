f61b79d2c2bf470c965bb1f495e587893fb1f1ff
A1rPun <thombakker@gmail.com>
2021-12-23 19:14:39 +0100

    :pencil: 2021Day 21 wip

--- 2021/day_21/solution.js
+++ 2021/day_21/solution.js
@@ -1,13 +1,34 @@
+import { getNumbers } from '../../js/december.js';
+
 export default {
-  title: '',
-  questions: ['', ''],
-  answer1: (puzzle) => {},
+  title: 'Dirac Dice',
+  questions: [
+    'What do you get if you multiply the score of the losing player by the number of times the die was rolled during the game?',
+    '',
+  ],
+  answer1: (puzzle) => {
+    const [,p1,,p2] = getNumbers(puzzle);
+    const scores = [0, 0];
+    const currentPlayer = 0;
+    const max = 1000;
+    const rolls = 0;
+
+    while (scores.every(x => x < max)) {
+
+      rolls++;
+    }
+
+    return rolls * Math.min(...scores);
+  },
   answer2: (puzzle) => {},
   example: [
-    // {
-    //   input: ``,
-    //   solutions: [],
-    // },
+    {
+      input: `Player 1 starting position: 4
+Player 2 starting position: 8`,
+      solutions: [],
+    },
   ],
+  input: `Player 1 starting position: 7
+Player 2 starting position: 2`,
   solutions: [],
 };
