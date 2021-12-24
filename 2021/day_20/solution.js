3f7486fdfcb1fac7e7e4735d8351e836a799097b
A1rPun <thombakker@gmail.com>
2021-12-24 16:15:52 +0100

    :star: 2021 Day 20 complete

--- 2021/day_20/solution.js
+++ 2021/day_20/solution.js
@@ -1,11 +1,37 @@
+import { createSquareGrid } from '../../js/december.js';
+
+const on = '#';
+
+function ENHANCE(img, lookup, times) {
+  return createSquareGrid(img.length + 2, (x, y) => {
+    const area = [-1, 0, 1]
+      .flatMap((i) => [-1, 0, 1].map((j) => [x - 1 + j, y - 1 + i]))
+      .map(([x, y]) => img[y]?.[x] ?? (lookup[0] === on && times % 2 ? on : ''))
+      .map((x) => (x === on ? 1 : 0))
+      .join('');
+    return lookup[parseInt(area, 2)];
+  });
+}
+
+function solve(puzzle, times = 2) {
+  const [lookup, image] = puzzle.split('\n\n');
+  let img = image.split('\n').map((x) => x.split(''));
+
+  for (let i = 0; i < times; i++) {
+    img = ENHANCE(img, lookup, i);
+  }
+  return img.reduce(
+    (acc, y) => acc + y.reduce((acc, x) => acc + (x === on ? 1 : 0), 0),
+    0
+  );
+}
+
 export default {
   title: 'Trench Map',
   questions: ['How many pixels are lit in the resulting image?', ''],
-  answer1: (puzzle) => {
-    const [enhancement, image] = puzzle.split('\n\n');
-    
-  },
-  answer2: (puzzle) => {},
+  solutions: [5391, 16383],
+  answer1: (puzzle) => solve(puzzle),
+  answer2: (puzzle) => solve(puzzle, 50),
   example: [
     {
       input: `..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
@@ -15,8 +41,7 @@ export default {
 ##..#
 ..#..
 ..###`,
-      solutions: [35],
+      solutions: [35, 3351],
     },
   ],
-  solutions: [],
 };
