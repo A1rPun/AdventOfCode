8840e7da42bde26a724df94bfeabe4eae3c54e04
A1rPun <thombakker@gmail.com>
2021-12-25 20:27:38 +0100

    :star: 2021 Day 15 complete

--- 2021/day_15/astar-weight.js
+++ 2021/day_15/astar-weight.js
@@ -1,38 +1,9 @@
-﻿export function manhattanDistance(point, goal) {
-  return Math.abs(point.x - goal.x) + Math.abs(point.y - goal.y);
-}
+﻿import { AStar, manhattanDistance, Point } from '../../js/astar.js';
 
-export const Point = (x, y) => ({ x, y });
-
-export class AStar {
-  constructor(grid, acceptableTiles) {
-    this.grid = grid || [[]];
-    this.acceptableTiles = acceptableTiles || [0];
-    this.worldMin = -1;
-    this.worldHeight = grid.length;
-    this.worldWidth = grid[0].length;
-    this.worldSize = this.worldHeight * this.worldWidth;
-  }
-
-  neighbours(x, y) {
-    const north = y - 1;
-    const east = x + 1;
-    const south = y + 1;
-    const west = x - 1;
-    const result = [];
-    const canWalkHere = (x, y) =>
-      this.acceptableTiles.find((tile) => tile === this.grid[y][x]);
-
-    if (north > this.worldMin && canWalkHere(x, north))
-      result.push(Point(x, north));
-    if (east < this.worldWidth && canWalkHere(east, y))
-      result.push(Point(east, y));
-    if (south < this.worldHeight && canWalkHere(x, south))
-      result.push(Point(x, south));
-    if (west > this.worldMin && canWalkHere(west, y))
-      result.push(Point(west, y));
-
-    return result;
+export class AStarWeight extends AStar {
+  constructor(grid, acceptableTiles, weight = 0) {
+    super(grid, acceptableTiles);
+    this.weight = weight;
   }
 
   findPath([x1, y1], [x2, y2], fn) {
@@ -74,10 +45,11 @@ export class AStar {
       } else {
         for (const neighbour of this.neighbours(node.x, node.y)) {
           let nextNode = Node(neighbour, node);
-
+          
           if (!aStar[nextNode.key]) {
-            nextNode.g = node.g + distanceFunction(neighbour, node);
-            nextNode.f = nextNode.g + distanceFunction(neighbour, pathEnd);
+            nextNode.g = node.g + distanceFunction(neighbour, node) * this.grid[nextNode.y][nextNode.x];
+            nextNode.h = nextNode.h ?? this.weight * distanceFunction(neighbour, pathEnd);
+            nextNode.f = nextNode.g + nextNode.h;
             open.push(nextNode);
             aStar[nextNode.key] = true;
           }
--- 2021/day_15/solution.js
+++ 2021/day_15/solution.js
@@ -1,22 +1,58 @@
-import { createSquareGrid, toInt } from '../../js/december.js';
-import { AStar } from '../../js/astar.js';
+import { createSquareGrid, toInt, log } from '../../js/december.js';
+import { AStarWeight } from './astar-weight.js';
+
+function getPath(puzzle) {
+  const grid = createSquareGrid(puzzle.length, (x, y) => toInt(puzzle[y][x]));
+
+  const aStar = new AStarWeight(grid, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  return aStar.findPath([0, 0], [puzzle.length - 1, puzzle.length - 1]);
+}
+
+function expandRow(row, n) {
+  return row
+    .split('')
+    .map((x) => Number(x) + n)
+    .map((x) => (x < 10 ? x : (x % 10) + 1))
+    .join('');
+}
+
+function expandGrid(grid) {
+  return [
+    [0, 1, 2, 3, 4],
+    [1, 2, 3, 4, 5],
+    [2, 3, 4, 5, 6],
+    [3, 4, 5, 6, 7],
+    [4, 5, 6, 7, 8],
+  ].flatMap((row) =>
+    grid.map((gridRow) =>
+      row.reduce((acc, x) => acc + expandRow(gridRow, x), '')
+    )
+  );
+}
+
+function getRisk(grid, path, first) {
+  return path.reduce((acc, [x, y]) => acc + toInt(grid[y][x]), -toInt(first));
+}
 
 export default {
   title: 'Chiton',
   questions: [
     'What is the lowest total risk of any path from the top left to the bottom right?',
-    '',
+    'What is the lowest total risk of any path from the top left to the bottom right?',
   ],
   answer1: (puzzle) => {
-    const check = puzzle.split('\n');
-    const grid = createSquareGrid(10, (x, y) => toInt(check[y][x]));
-
-    const aStar = new AStar(grid, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    const path = aStar.findPath([0, 0], [9, 9]);
-
-    return path.length;
+    const grid = puzzle.split('\n');
+    const path = getPath(grid);
+    log(prettify(grid, path));
+    return getRisk(grid, path, puzzle[0]);
   },
-  // answer2: (puzzle) => {},
+  answer2: (puzzle) => {
+    const grid = expandGrid(puzzle.split('\n'));
+    const path = getPath(grid);
+    return getRisk(grid, path, puzzle[0]);
+  },
+  solutions: [508, 2872],
+  tries: [['607 too high', '512 too high']],
   example: [
     {
       input: `1163751742
@@ -29,8 +65,60 @@ export default {
 3125421639
 1293138521
 2311944581`,
-      solutions: [40],
+      solutions: [40, 315],
+    },
+    {
+      input: `1111111111
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+2111111111`,
+      solutions: [18],
+    },
+    {
+      input: `1111111112
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+1999999991
+1111111111`,
+      solutions: [18],
+    },
+    {
+      input: `1234567899
+2123456789
+3212345678
+4321234567
+5432123456
+6543212345
+7654321234
+8765432123
+9876543212
+9987654321`,
+      solutions: [27],
     },
   ],
-  solutions: [],
 };
+
+function prettify(grid, path) {
+  let result = '';
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      const isPath = path.find(([xx, yy]) => xx === x && yy === y);
+      result += isPath
+        ? `${String.fromCodePoint(8320 + toInt(grid[y][x]))}`
+        : `${grid[y][x]}`;
+    }
+    result += '\n';
+  }
+  return result;
+}
