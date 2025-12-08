ae800c51611c011d9be8efadcdf825f86f22be57
A1rPun <thombakker@gmail.com>
2025-12-08 20:29:00 +0100

    :star: 2025 Day 5 example

--- 2025/day_05/solution.js
+++ 2025/day_05/solution.js
@@ -1,24 +1,28 @@
 import { toInt } from '../../js/december.js';
 
 export default {
-  title: 'aabbb',
+  title: 'Cafeteria',
   questions: [
-    '',
-    '',
+    'How many of the available ingredient IDs are fresh?',
+    'How many ingredient IDs are considered to be fresh according to the fresh ingredient ID ranges?',
   ],
-  answer1: (puzzle) => {
-    
-    return puzzle;
-  },
-  answer2: (puzzle) => {
-    
-    return puzzle;
-  },
+  answer1: (puzzle) => {},
+  answer2: (puzzle) => {},
   example: [
     {
-      input: ``,
-      solutions: [],
+      input: `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`,
+      solutions: [3, 14],
     },
   ],
-  solutions: [],
+  solutions: [896],
 };
