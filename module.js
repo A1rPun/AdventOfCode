c3c99bd1a18abdf7ab7a23ba8a7669b67a0912fb
A1rPun <thombakker@gmail.com>
2025-12-08 20:50:31 +0100

    :star: 2022 + 2023

--- module.js
+++ module.js
@@ -5,6 +5,8 @@ import days2018 from './2018/module.js';
 import days2019 from './2019/module.js';
 import days2020 from './2020/module.js';
 import days2021 from './2021/module.js';
+import days2022 from './2022/module.js';
+import days2023 from './2023/module.js';
 import days2024 from './2024/module.js';
 import days2025 from './2025/module.js';
 
@@ -16,6 +18,8 @@ export default [
   days2019,
   days2020,
   days2021,
+  days2022,
+  days2023,
   days2024,
   days2025,
 ];
